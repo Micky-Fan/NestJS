5085cf4c9b84f477a09d15cafa6d7645d724aa6c
Micky <sdps40105@gmail.com>
2023-02-20 21:53:34 +0800

    feat: override the ThrottlerGuard to chekc user id and ip in the same guard

---
+++ src/data/user.throttler.guard.ts
@@ -0,0 +1,28 @@
+import { ThrottlerGuard } from '@nestjs/throttler';
+import {
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+} from '@nestjs/common';
+
+@Injectable()
+export class userThrottlerGuard extends ThrottlerGuard {
+  async handleRequest(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const id = req.query.user;
+    const ip = req.connection.remoteAddress;
+    const idKey = this.generateKey(context, id);
+    const ipKey = this.generateKey(context, ip);
+    const idStorage = await this.storageService.increment(idKey, 60);
+    const ipStorage = await this.storageService.increment(ipKey, 60);
+    if (idStorage.totalHits > 5 || ipStorage.totalHits > 10) {
+      throw new HttpException(
+        { ip: ip, id: idStorage.totalHits },
+        HttpStatus.TOO_MANY_REQUESTS,
+      );
+    }
+
+    return true;
+  }
+}
