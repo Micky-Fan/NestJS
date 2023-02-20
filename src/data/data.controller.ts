991bbcfa42c2efb8e6287d594163c147637c41a6
Micky <sdps40105@gmail.com>
2023-02-20 21:55:38 +0800

    feat: import throttlerGuard into data.controller and data.module

--- src/data/data.controller.ts
+++ src/data/data.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { DataService } from './data.service';
+import { userThrottlerGuard } from './user.throttler.guard';
 
 @Controller('data')
 export class DataController {
   constructor(private readonly dataService: DataService) {}
+  @UseGuards(userThrottlerGuard)
   @Get()
-  async getHackerNews() {
+  async getHackerNews(@Query('user') id: number) {
+    console.log('id==>', id);
     return this.dataService.getHackerNews();
   }
 }
--- src/data/data.module.ts
+++ src/data/data.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { DataController } from './data.controller';
 import { DataService } from './data.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
   controllers: [DataController],
   providers: [DataService],
 })
